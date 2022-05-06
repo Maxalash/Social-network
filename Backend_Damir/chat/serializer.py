3f239d54e54c24cc880dce0c524b4bd9e08c95b6
Makhmut <mahmudlyder@gmail.com>
2022-05-06 14:53:34 +0600

    feat; yeah doesn't matter

--- Backend_Damir/chat/serializer.py
+++ Backend_Damir/chat/serializer.py
@@ -1,44 +1,36 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import *
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','id']
-
 class MessageSerializer(serializers.ModelSerializer):
     yours = serializers.SerializerMethodField()
-
+    owner = serializers.SerializerMethodField()
     class Meta:
         model = Message
         fields = '__all__'
-
     def get_yours(self, obj):
         user = None
         request = self.context.get("request")
         if request and hasattr(request, "user"):
             user = request.user
         return True if user == obj.owner else False
-
+    def get_owner(self,obj):
+        return obj.owner.username
 class MessageSendSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ['text','owner','chat']
-
-
 class MessageLoadSerializer(serializers.ModelSerializer):
     messages = serializers.SerializerMethodField()
-
     class Meta:
         model = Chat
         fields = '__all__'
-
     def get_messages(self, obj):
         messages = Message.objects.filter(chat=obj)
         return MessageSerializer(messages,many = True).data
-
 # class UsernameField(serializers.RelatedField):
 #     def to_representation(self, obj):
 #         user = None
@@ -47,8 +39,6 @@ class MessageLoadSerializer(serializers.ModelSerializer):
 #             user = request.user
 #         if obj.user != user:
 #             return user
-
-
 class ChatSerializer(serializers.ModelSerializer):
     friend = serializers.SerializerMethodField()
     user = serializers.SlugRelatedField(
@@ -57,17 +47,14 @@ class ChatSerializer(serializers.ModelSerializer):
         slug_field='username'
     )
     # user = UsernameField(many=True)
-
     class Meta:
         model = Chat
         fields = '__all__'
     def get_friend(self,obj):
-
         # print(obj.user)
         # for i in obj.user:
         #     print(i)
         return str(obj.user)
-
 class CreateChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
